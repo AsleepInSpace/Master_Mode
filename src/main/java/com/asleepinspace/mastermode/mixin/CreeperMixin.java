c2f51d2c9c47dfb6bf680e403206466ed54f439e
AsleepInSpace <Johnaidr2017@gmail.com>
2023-08-04 23:38:58 -0400

    Changed the way the creeperMixin worked so that it would not break compatibility with other mods.

--- src/main/java/com/asleepinspace/mastermode/mixin/CreeperMixin.java
+++ src/main/java/com/asleepinspace/mastermode/mixin/CreeperMixin.java
@@ -4,21 +4,18 @@ package com.asleepinspace.mastermode.mixin;
 
 import net.minecraft.world.entity.monster.Creeper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 
 
 @Mixin(Creeper.class)
-public class CreeperMixin {
+public interface CreeperMixin {
+    @Accessor("explosionRadius")
+    public void setExplosionRadius(int explosionRadius);
 
 
-    @ModifyArg( method = "explodeCreeper()V", at = @At(value = "INVOKE",
-    target = "Lnet/minecraft/world/level/Level;explode(Lnet/minecraft/world/entity/Entity;DDDFLnet/minecraft/world/level/Explosion$BlockInteraction;)Lnet/minecraft/world/level/Explosion;" ),
-    index = 4)
-    public float ExplosionRadius( float F) {
-        return F*2;
 
-    }
+
+
 
 }
