d69afceda2058de8d172ff4aa3d10e1bc506fe10
AsleepInSpace <Johnaidr2017@gmail.com>
2023-08-04 23:39:52 -0400

    Changed the attribute system to add an nbt value depending on the world hardness and only give certain attributes depending on how hard the world is. (Changes are still being worked on)

--- src/main/java/com/asleepinspace/mastermode/event/ModEvents.java
+++ src/main/java/com/asleepinspace/mastermode/event/ModEvents.java
@@ -1,10 +1,14 @@
 package com.asleepinspace.mastermode.event;
 
 import com.asleepinspace.mastermode.gamerules.ModGamerules;
+import com.asleepinspace.mastermode.mixin.CreeperMixin;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,12 +18,19 @@ import static com.asleepinspace.mastermode.util.values.AttributeValues.*;
 
 
 @Mod.EventBusSubscriber
-public class ModEvents {
+public class ModEvents  {
+
+
+
 
 
     @SubscribeEvent
     public static void LivingJoins (LivingSpawnEvent.SpecialSpawn event) {
+        event.getEntity().getPersistentData().putInt("Hardmode_Level", event.getEntity().level.getGameRules().getInt(ModGamerules.ATTRIBUTE_LEVELS));
         if (event.getEntity() instanceof Monster && event.getEntity().level.getGameRules().getInt(ModGamerules.ATTRIBUTE_LEVELS) == 2) {
+            if (event.getEntity() instanceof Creeper) {
+                ((CreeperMixin) event.getEntity()).setExplosionRadius(4);
+            }
             AttributeInstance Max_health = event.getEntity().getAttribute(Attributes.MAX_HEALTH);
             AttributeInstance Max_speed = event.getEntity().getAttribute(Attributes.MOVEMENT_SPEED);
             AttributeInstance Knockback_resistance = event.getEntity().getAttribute(Attributes.KNOCKBACK_RESISTANCE);
@@ -30,6 +41,20 @@ public class ModEvents {
             Max_speed.addPermanentModifier(SPEED_BONUS);
 
             event.getEntity().setHealth(event.getEntity().getMaxHealth());
+        }
+
+
+    }
+
+    @SubscribeEvent
+    public static void EntityTick (LivingEvent.LivingTickEvent event) {
+        if (event.getEntity() instanceof Monster &&
+                event.getEntity().getPersistentData().getInt("Hardmode_Level") !=
+                event.getEntity().level.getGameRules().getInt(ModGamerules.ATTRIBUTE_LEVELS)) {
+
+
+            event.getEntity().getPersistentData().putInt("Hardmode_Level",
+                    event.getEntity().level.getGameRules().getInt(ModGamerules.ATTRIBUTE_LEVELS));
         }
 
 
